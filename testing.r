d671bf14d09da741aff74c5f6726b156cd49c4e1
Naqeeb Ashraf <naqeeb_memon@hotmail.com>
2022-11-03 10:31:22 +0500

    testing

--- testing.r
+++ testing.r
@@ -1,11 +1,1 @@
-# This is from Github
-print("Hello from Github")
-
-name="Naqeeb"
-version="Rstudio - in writing"
-
-print(name)
-print("These lines has been writtern in Rstudio")
-print("i will try")
-print("why its not save?")
-
+print("hello")
